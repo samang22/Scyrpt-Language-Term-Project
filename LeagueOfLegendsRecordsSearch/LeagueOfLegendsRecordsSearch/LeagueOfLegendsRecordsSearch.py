ffddc90472568eadda67d229bb33aa1fd2b9b5e6
samang22 <samang22@naver.com>
2019-05-28 20:35:03 +0900

    매치데이터 불러오기 성공
    헤더의 길이정보가 없어서 어쩔 수 없이 상수넣어서 해결함, 정확한 해결방법은 잘 모르겠음
    이제 매치 고유의 게임아이디로 검색하여 해당 매치의 세부정보를 받아와야함

--- LeagueOfLegendsRecordsSearch/LeagueOfLegendsRecordsSearch/LeagueOfLegendsRecordsSearch.py
+++ LeagueOfLegendsRecordsSearch/LeagueOfLegendsRecordsSearch/LeagueOfLegendsRecordsSearch.py
@@ -38,8 +38,7 @@ my_api_key = "RGAPI-20f33bb9-dd69-4562-a966-0440eb963870"
 # leaguePoints": 23
 # 자유랭 반복
 
-#my_api_key = "RGAPI-20f33bb9-dd69-4562-a966-0440eb963870"
-my_api_key = "RGAPI-a5622a8d-de79-44cc-841f-5426b231542f"
+my_api_key = "RGAPI-1d01a975-e32d-4e86-8e44-0f90de28f81a"
 class LOLAPIProcess:
     def GetSummonerName(self, SummonerName):
 
@@ -60,22 +59,42 @@ class LOLAPIProcess:
         server = "kr.api.riotgames.com"  
 
         summoner_url = "/lol/summoner/v4/summoners/by-name/" + ParsedSummonerName + "?api_key=" + my_api_key
-        conn = http.client.HTTPSConnection(server)
+        self.conn = http.client.HTTPSConnection(server)
 
-        conn.request("GET", summoner_url)
-        response = conn.getresponse()
+        self.conn.request("GET", summoner_url)
+        response = self.conn.getresponse()
+        if 200 != response.code: 
+            print("없는 소환사명입니다.")
+            return
         cLen = response.getheader("Content-Length")  # 헤더에서 Content-Length 즉 얼만큼 읽었는지 추출
         result = response.read(int(cLen)).decode('utf-8')
 
         self.SummonerJsonData = ParsingSummonerData(result) #파싱한 JSON 데이터를 요소별로 객체에 저장ㅇ한 클래스
-
+    
+        
+    def GetSummonerLeagueData(self):
         summoner_league_url = "/lol/league/v4/positions/by-summoner/" + self.SummonerJsonData.id + "?api_key=" + my_api_key
-        conn.request("GET", summoner_league_url)
-        response = conn.getresponse()
+        self.conn.request("GET", summoner_league_url)
+        response = self.conn.getresponse()
         cLen = response.getheader("Content-Length")  # 헤더에서 Content-Length 즉 얼만큼 읽었는지 추출
         result = response.read(int(cLen)).decode('utf-8')
-
         self.SummonerLeagueJsonData = ParsingSummonerLeagueData(result)
+
+    def GetSummonerMatchData(self):
+        summoner_match_url = "/lol/match/v4/matchlists/by-account/" + self.SummonerJsonData.accountId +"?api_key="+my_api_key
+        self.conn.request("GET", summoner_match_url)
+        response = self.conn.getresponse()
+        cLen = response.getheader("Content-Length")  # 헤더에서 Content-Length 즉 얼만큼 읽었는지 추출
+        #13523
+
+        #if cLen != None:
+        #    result = response.read(int(cLen)).decode('utf-8')
+        #    self.SummonerLeagueJsonData = ParsingSummonerMatchData(result)
+        #else:
+        #    print("매치데이터 읽어오기 실패")
+        result = response.read(15000).decode('utf-8')
+        self.SummonerMatchJsonData = ParsingSummonerMatchData(result)
+
 
 
 	##이미지 파일 얻어오는코드
@@ -94,6 +113,8 @@ class LOLAPIProcess:
     def PrintSummonerLeagueData(self):
         print(self.SummonerLeagueJsonData)
 
+    def PrintSummonerMatchData(self):
+        print(self.SummonerMatchJsonData)
 
 
 class ParsingSummonerData:
@@ -124,7 +145,7 @@ class ParsingSummonerData:
             "]\n[name : " + self.name + \
             "]\n[profileIconId : " + self.profileIconId + \
             "]\n[revisionData : " + self.revisionDate + \
-            "]\n[summonerLevel : " + self.summonerLevel + "]" 
+            "]\n[summonerLevel : " + self.summonerLevel + "]\n\n" 
 
     def getid(self):
         return self.id
@@ -179,12 +200,12 @@ class ParsingSummonerLeagueData:
             "]\n[hotStreak : " + self.solo_hotStreak + \
             "]\n[wins : " + self.solo_wins + \
             "]\n[losses : " + self.solo_losses + \
-            "]\n[rank" +  self.solo_rank + \
+            "]\n[rank : " +  self.solo_rank + \
             "]\n[leagueName : " + self.solo_leagueName + \
             "]\n[leagueId : " + self.solo_leagueId + \
             "]\n[queueType : " + self.solo_queueType + \
             "]\n[summonerId : " + self.solo_summonerId + \
-            "]\n[leaguePoints : " + self.solo_leaguePoints + "]\n"
+            "]\n[leaguePoints : " + self.solo_leaguePoints + "]\n\n"
             #"]\n[leaguePoints : " + self.solo_leaguePoints + "]\n" + \
             #"[free]\n" + \
             #"[tier : " + self.solo_tier + \
@@ -199,15 +220,99 @@ class ParsingSummonerLeagueData:
             #"]\n[summonerId : " + self.solo_summonerId + \
             #"]\n[leaguePoints : " + self.solo_leaguePoints + "]"
 
+class ParsingSummonerMatchData:
+    def __init__(self, JSON):
+        self.jsonData = json.loads(JSON) #string 형태의 JSON 객체를 딕셔너리로 바꾼다
+
+        #self.platformId = self.jsonData[0]["platformId"]
+        #self.gameId = str(self.jsonData[0]["gameId"])
+        #self.champion = str(self.jsonData[0]["champion"])
+        #self.queue = str(self.jsonData[0]["queue"])
+        #self.season = str(self.jsonData[0]["season"])
+        #self.timestamp = str(self.jsonData[0]["timestamp"])
+        #self.role = str(self.jsonData[0]["role"])
+        #self.lane = str(self.jsonData[0]["lane"])
+
+        self.platformId = []
+        self.gameId = []
+        self.champion = []
+        self.queue = []
+        self.season = []
+        self.timestamp = []
+        self.role = []
+        self.lane = []
+
+
+        for n in range(int(self.jsonData["endIndex"])):
+            self.platformId.append(self.jsonData["matches"][n]["platformId"])
+            self.gameId.append(str(self.jsonData["matches"][n]["gameId"]))
+            self.champion.append(str(self.jsonData["matches"][n]["champion"]))
+            self.queue.append(str(self.jsonData["matches"][n]["queue"]))
+            self.season.append(str(self.jsonData["matches"][n]["season"]))
+            self.timestamp.append(str(self.jsonData["matches"][n]["timestamp"]))
+            self.role.append(str(self.jsonData["matches"][n]["role"]))
+            self.lane.append(str(self.jsonData["matches"][n]["lane"]))
+
+
+    def __str__(self):
+        return \
+            "[platformId : " + self.platformId[0] + \
+            "]\n[gameId : " + self.gameId[0] + \
+            "]\n[champion : " + self.champion[0] + \
+            "]\n[queue : " + self.queue[0] + \
+            "]\n[season : " + self.season[0] + \
+            "]\n[timestamp" +  self.timestamp[0] + \
+            "]\n[role : " + self.role[0] + \
+            "]\n[lane : " + self.lane[0] + "]\n" \
+                + \
+            "[platformId : " + self.platformId[1] + \
+            "]\n[gameId : " + self.gameId[1] + \
+            "]\n[champion : " + self.champion[1] + \
+            "]\n[queue : " + self.queue[1] + \
+            "]\n[season : " + self.season[1] + \
+            "]\n[timestamp" +  self.timestamp[1] + \
+            "]\n[role : " + self.role[1] + \
+            "]\n[lane : " + self.lane[1] + "]\n" \
+                + \
+            "[platformId : " + self.platformId[2] + \
+            "]\n[gameId : " + self.gameId[2] + \
+            "]\n[champion : " + self.champion[2] + \
+            "]\n[queue : " + self.queue[2] + \
+            "]\n[season : " + self.season[2] + \
+            "]\n[timestamp" +  self.timestamp[2] + \
+            "]\n[role : " + self.role[2] + \
+            "]\n[lane : " + self.lane[2] + "]\n" \
+                + \
+            "[platformId : " + self.platformId[3] + \
+            "]\n[gameId : " + self.gameId[3] + \
+            "]\n[champion : " + self.champion[3] + \
+            "]\n[queue : " + self.queue[3] + \
+            "]\n[season : " + self.season[3] + \
+            "]\n[timestamp" +  self.timestamp[3] + \
+            "]\n[role : " + self.role[3] + \
+            "]\n[lane : " + self.lane[3] + "]\n" \
+                + \
+            "[platformId : " + self.platformId[4] + \
+            "]\n[gameId : " + self.gameId[4] + \
+            "]\n[champion : " + self.champion[4] + \
+            "]\n[queue : " + self.queue[4] + \
+            "]\n[season : " + self.season[4] + \
+            "]\n[timestamp" +  self.timestamp[4] + \
+            "]\n[role : " + self.role[4] + \
+            "]\n[lane : " + self.lane[4] + "]\n\n" \
+            
+
 
 
 
 Process = LOLAPIProcess()
 summoner_name = str(input("소환사 이름 입력 : "))
 Process.GetSummonerName(summoner_name)
-#Process.GetSummonerName("죽기장인")
 Process.GetSummonerInfo()
+Process.GetSummonerLeagueData()
+Process.GetSummonerMatchData()
 Process.PrintSummonerData()
 Process.PrintSummonerLeagueData()
+Process.PrintSummonerMatchData()
 
 
